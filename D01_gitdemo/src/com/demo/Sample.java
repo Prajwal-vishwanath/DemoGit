d45ec770fbb214a9c8c6b71932b1e15d92b13e5a
Prajwal <Prajwal@LAPTOP-C5OH21RL>
2020-12-18 14:35:06 +0530

    added Sub method in sample.java file

--- D01_gitdemo/src/com/demo/Sample.java
+++ D01_gitdemo/src/com/demo/Sample.java
@@ -5,4 +5,8 @@ public class Sample {
 	public void add(int num1, int num2) {
 		System.out.println("Sum="+(num1+num2));
 	}
+	
+	public void sub(int num1, int num2) {
+		System.out.println("sub="+(num1-num2));
+	}
 }
